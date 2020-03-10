f5191c8eeecd488a4e2d68eccbe55b6a8469e647
Viarow <universe37@outlook.com>
2020-03-10 22:35:08 +0800

    second commit

--- train.py
+++ train.py
@@ -7,6 +7,31 @@ import torch.optim as optim
 import argparse
 from model import ResNet50
 
+FLOP_list = []
+
+def compute_flops(module, input, output):
+
+    c_in = input[0].shape[1]
+    k_size = 3
+    h, w = input[0].shape[2], input[0].shape[3]
+    batch_size = output[0].shape[0]
+    flops = 0
+
+    for k in range(0, batch_size):
+        c_out = output[0][k].cpu().sum()
+        flops += ((k_size * k_size * c_in) * c_out + c_out) * (h * w)
+        FLOP_list.append(flops)
+
+
+def register_layers(model):
+    handles = []
+    for idx, m in enumerate(model.named_modules()):
+        module_name = m[0]
+        if module_name.split('.')[-1] == 'channel_gate':
+            handle = m[1].register_forward_hook(compute_flops)
+            handles.append(handle)
+    return handles
+
 
 def train(args):
 
@@ -24,7 +49,6 @@ def train(args):
     model = ResNet50(num_classes=10)
     print('Model built.')
 
-
     if torch.cuda.device_count()>1:
         model = nn.DataParallel(model).cuda()
         print('Model data parallel to cuda.')
@@ -32,7 +56,7 @@ def train(args):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         model = model.to(device)
 
-
+    handles = register_layers(model)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum,
@@ -49,19 +73,24 @@ def train(args):
             inputs, labels = inputs.cuda(), labels.cuda()
 
             optimizer.zero_grad()
-
             outputs = model(inputs)
-            #flops = model.flops
-            #compute_cost = args.alpha * flops
-            loss = criterion(outputs, labels)
+
+            for handle in handles:
+                handle.remove()
+            handles = []
+            compute_cost = sum(FLOP_list) * args.alpha
+
+            loss = criterion(outputs, labels) + torch.Tensor([compute_cost]).cuda()
+            #loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
-            if i % 2000 == 1999:
-                #print("epoch-%d sample-%d compute_cost: %.3f" % (epoch+1, i+1, compute_cost))
-                print("epoch-%d sample-%d running_loss: %.3f" % (epoch + 1, i + 1, running_loss))
+
+            if i % args.checkpoint == (args.checkpoint-1) :
+                print("epoch-%d sample-%d running_loss: %.3f" % (epoch + 1, i + 1, running_loss/args.checkpoint))
                 running_loss = 0.0
+
 
 
 def parse_args():
@@ -72,7 +101,8 @@ def parse_args():
     parser.add_argument('-lr', type=float, default=5e-2)
     parser.add_argument('-weight_decay', type=float, default=1e-4)
     parser.add_argument('-batch_size', type=int, default=4)
-    parser.add_argument('-alpha', type=float, default=2e-4)
+    parser.add_argument('-alpha', type=float, default=1e-11)
+    parser.add_argument('-checkpoint', type=int, default=50)
     args = parser.parse_args()
 
     return args
