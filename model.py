f1e18701cc927c25c6112389d6e3e26a8e2d5fcc
Viarow <universe37@outlook.com>
2020-03-10 12:57:13 +0800

    second commit

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import torch.nn.functional
@@ -6,6 +5,7 @@ import torch.nn.functional
 
 def conv_flops(c_in, c_out, k_size, h, w):
     return ((k_size * k_size * c_in) * c_out + c_out) * (h * w)
+
 
 class LayerGate(nn.Module):
 
@@ -18,6 +18,7 @@ class LayerGate(nn.Module):
         # blob shape: N*10*1*1, N refers to batch size
         self.lstm = nn.LSTM(input_size=10, hidden_size=10, num_layers=1)
         self.fc = nn.Linear(10, 1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, input):
         x = self.GAP(input)
@@ -26,13 +27,13 @@ class LayerGate(nn.Module):
 
         #seq_len = 1
         x = x.view(1, -1, 10)
+        self.lstm.flatten_parameters()
         x_lstm, (hn, cn) = self.lstm(x)
 
         # x_lstm shape = 1*N*10
         x = self.fc(x_lstm.view(-1, 10))
         x = self.relu(x)
-
-        x = nn.functional.sigmoid(x)
+        x = self.sigmoid(x)
         output = torch.round(x)
 
         return output
@@ -42,12 +43,12 @@ class ChannelGate(nn.Module):
 
     def __init__(self, in_channels):
         super().__init__()
-
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=in_channels,
                               kernel_size=3, stride=2)
         self.GAP = nn.AdaptiveAvgPool2d((1,1))
         self.fc = nn.Linear(in_channels, in_channels)
         self.relu = nn.ReLU(inplace=True)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, input):
         x = self.conv(input)
@@ -55,7 +56,7 @@ class ChannelGate(nn.Module):
         x = self.GAP(x)
         x = self.fc(torch.squeeze(x))
         x = self.relu(x)
-        x = nn.functional.sigmoid(x)
+        x = self.sigmoid(x)
         output = torch.round(x)
 
         return output
@@ -65,27 +66,21 @@ class AdaptConv2d(nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size=3,
                  stride=1, padding=1, bias=False):
-
-        """ in_channels equals out_channels in this case. """
-
         super().__init__()
-        self.flops = 0
         self.layer_gate = LayerGate(in_channels)
         self.channel_gate = ChannelGate(in_channels)
         self.conv = nn.Conv2d(in_channels, out_channels, stride=stride,
-                              kernel_size=kernel_size, padding=padding, bias=bias),
-
+                              kernel_size=kernel_size, padding=padding, bias=bias)
 
     def forward(self, input):
 
         layer_idx = self.layer_gate(input)
         # shape of layer_idx: N*1
-
         channel_idx = self.channel_gate(input)
         # shape of channel_idx: N*C
 
         batch_size = input.shape[0]
-        output = torch.zeros(input.shape)
+        output = torch.zeros(input.shape).cuda()
 
         for k in range(0, batch_size):
 
@@ -99,110 +94,87 @@ class AdaptConv2d(nn.Module):
                 idx = channel_idx[k].view(1,-1,1,1)
                 output[k] = idx*image_conv + (1-idx)*image
 
-                self.flops += conv_flops(c_in=input.shape[1],c_out=channel_idx[k].sum(),
-                                         k_size=self.conv.kernel_size, h=input.shape[-2], w=input.shape[-1])
-
         return output
 
 
 class ResidualBlock(nn.Module):
 
-    def __init__(self, in_channels,  out_channels, stride=1):
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.scale = int(4/stride)
-        self.flops = 0
-        self.residual_pre = nn.Sequential(
+
+        self.residual_function = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0, stride=stride, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-        self.adapt_conv =  AdaptConv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.residual_post = nn.Sequential(
+            nn.ReLU(inplace=True),
+            AdaptConv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels*self.scale, kernel_size=1, padding=0, stride=1, bias=False),
-            nn.BatchNorm2d(out_channels*self.scale)
+            nn.Conv2d(out_channels, out_channels * ResidualBlock.expansion, kernel_size=1, padding=0, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels * ResidualBlock.expansion),
         )
 
         self.shortcut = nn.Sequential()
 
-        if stride!=1 or in_channels!=out_channels*self.scale:
+        if stride != 1 or in_channels != out_channels * ResidualBlock.expansion:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels*self.scale,
+                nn.Conv2d(in_channels, out_channels * ResidualBlock.expansion,
                           kernel_size=1, padding=0, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels*self.scale)
+                nn.BatchNorm2d(out_channels * ResidualBlock.expansion)
             )
 
     def forward(self, input):
-        x = self.residual_pre(input)
-        x = self.adapt_conv(x)
-        self.flops += self.adapt_conv.flops
-        x = self.residual_post(x)
-        y = x + self.shortcut(input)
-        output = nn.ReLU(inplace=True)(y)
+        x = self.residual_function(input) + self.shortcut(input)
+        output = nn.ReLU(inplace=True)(x)
 
         return output
-
-
-class ResidualStage(nn.Module):
-
-    def __init__(self, input_channels, output_channels, num_blocks, stride):
-        super().__init__()
-        self.flops = 0
-        strides = [stride] + [1] * (num_blocks-1)
-        self.blocks = []
-        in_channels = input_channels
-        out_channels = output_channels
-        for stride in strides:
-            self.blocks.append(ResidualBlock(in_channels, out_channels, stride))
-            in_channels = out_channels
-
-    def forward(self, x):
-        for block in self.blocks:
-            x = block(x)
-            self.flops += block.flops
-
-        return x
-
 
 
 class ResNet50(nn.Module):
 
     def __init__(self, num_classes=10):
         super().__init__()
-        self.flops = 0
+        self.in_channels = 64
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, padding=3, stride=2),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
         )
-        self.conv2 = ResidualStage(64, 256, 3, stride=1)
-        self.conv3 = ResidualStage(256, 512, 4, stride=2)
-        self.conv4 = ResidualStage(512, 1024, 6, stride=2)
-        self.conv5 = ResidualStage(1024, 2048, 3, stride=2)
-        self.pool = nn.AvgPool2d(kernel_size=7, stride=1)
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv2 = self.make_block(64, 3, stride=1)
+        self.conv3 = self.make_block(128, 4, stride=2)
+        self.conv4 = self.make_block(256, 6, stride=2)
+        self.conv5 = self.make_block(512, 3, stride=2)
+        self.avg_pool = nn.AvgPool2d(kernel_size=7, stride=1)
         self.fc = nn.Linear(2048, num_classes)
+
+
+    def make_block(self, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        blocks = []
+        for stride in strides:
+            blocks.append(ResidualBlock(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * ResidualBlock.expansion
+
+        return nn.Sequential(*blocks)
+
 
     def forward(self, input):
         x = self.conv1(input)
-
+        x = self.max_pool(x)
         x = self.conv2(x)
-        self.flops += self.conv2.flops
-
         x = self.conv3(x)
-        self.flops += self.conv3.flops
-
         x = self.conv4(x)
-        self.flops += self.conv4.flops
-
         x = self.conv5(x)
-        self.flops += self.conv5.flops
-
-        x = self.pool(x)
+        x = self.avg_pool(x)
         x = x.view(x.size(0), -1)
         output = self.fc(x)
 
         return output
+
+
+
 
 
 
